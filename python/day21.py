01a800c740acd3b8b5d18c5e8a1b20b427269a4e
ogmobot <ogmobot@github.com>
2024-05-16 17:43:09 +1000

    Python solutions: remove prototype day 21 solution, and rename successful one

--- python/day21.py
+++ python/day21.py
@@ -15,6 +15,10 @@ lines_ = """...........
 .##..##.##.
 ...........""".split("\n")
 
+lines_ = """...
+.S.
+..#""".split("\n")
+
 max_r = 0
 max_c = 0
 grid = {}
@@ -31,175 +35,150 @@ for row, line in enumerate(lines):
 height = max_r + 1
 width = max_c + 1
 
-def search_for_limit(grid, start_point, limit):
-    # mutates grid
-    distances = [{start_point: 1}] # values are the number of sub-grids counted
-    i = 0
-    #seen_configs = set()
-    while i < limit:
-        seen = set()
-        #if i % 100 == 0: print(i, "...")
-        distances.append(defaultdict(int))
-        start_points = distances[i]
-        for sp, count in start_points.items():
-            r, c = sp
-            for dr, dc in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
-                if ((r + dr), (c + dc)) in seen:
-                    continue
-                seen.add(((r + dr), (c + dc)))
-                # screws up at corners
-                if grid[(((r + dr) % height), ((c + dc) % width))] == '.':
-                    distances[i + 1][(((r + dr) % height), ((c + dc) % width))] += count
-        i += 1
-    return distances
-
-def even_odd_floodfill(grid, start_point, limit):
+def floodfill(orig_grid, start, limit):
+    grid = orig_grid.copy()
+    front = {start}
     timer = 0
-    evens = {start_point}
-    evens_total = 0
-    
-    odds = set()
-    odds_total = 0
-    
-    while timer < limit:
-        #if timer % 100 == 0: print(timer, '...', f'{len(odds)=} {len(evens)=}')
-        if timer % 2 == 0:
-            start_points = evens
-            other = odds
-        else:
-            start_points = odds
-            other = evens
+    while timer <= limit:
         new_front = set()
-        for sp in start_points:
-            #print(sp)
-            r, c = sp
-            for dr, dc in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
-                if (
-                    (((r + dr), (c + dc)) not in other)
-                    and (grid[(((r + dr) % height), ((c + dc) % width))] == '.')
-                ):
-                    new_front.add((r + dr, c + dc))
-        if timer % 2 == 0:
-            odds_total += len(odds)
-            odds = new_front
-        else:
-            evens_total += len(evens)
-            evens = new_front
-        
+        #print(front)
+        for r, c in front:
+            #print(r, c)
+            grid[(r, c)] = {0: 'E', 1: 'O'}[timer % 2]
+            #print_grid(grid)
+            for dr, dc in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+                new_r = r + dr
+                new_c = c + dc
+                if (new_r, new_c) not in grid:
+                    grid[(new_r, new_c)] = orig_grid[(new_r % height, new_c % width)]
+                if grid[(new_r, new_c)] == '.':
+                    new_front.add((new_r, new_c))
+        front = new_front
         timer += 1
-    return (len(evens) + evens_total, len(odds) + odds_total)
+                    
+    return grid
 
-def approximate(grid, limit):
-    hashes = list(grid.values()).count('#')
-    grid_area = height * width
-    return limit*limit*(1 - (hashes / grid_area))
+def count_of(piece, val):
+    return list(piece.values()).count(val)
 
-def time_to_reach_edges(grid, start_point):
-    # target is edges
-    timer = 0
-    evens = {start_point}
-    evens_total = 0
-    
-    odds = set()
-    odds_total = 0
+def print_grid(g):
+    min_r = min(r for (r, _) in g)
+    max_r = max(r for (r, _) in g)
+    min_c = min(c for (_, c) in g)
+    max_c = max(c for (_, c) in g)
+    for r in range(min_r, max_r + 1):
+        for c in range(min_c, max_c + 1):
+            print(g.get((r, c), ' '), end=' ')
+        print()
+    return
 
-    edges = dict()
-    while len(edges) < 10:
-        #if timer % 100 == 0: print(timer, '...', f'{len(odds)=} {len(evens)=}')
-        if timer % 2 == 0:
-            start_points = evens
-            other = odds
-        else:
-            start_points = odds
-            other = evens
-        new_front = set()
-        for sp in start_points:
-            #print(sp)
-            r, c = sp
-            if ((r, c) not in grid) and ((r, c) not in edges):
-                edges[(r, c)] = timer
-                continue
-            for dr, dc in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
-                if (
-                    (((r + dr), (c + dc)) not in other)
-                    and (grid[(((r + dr) % height), ((c + dc) % width))] == '.')
-                ):
-                    new_front.add((r + dr, c + dc))
-        if timer % 2 == 0:
-            odds_total += len(odds)
-            odds = new_front
-        else:
-            evens_total += len(evens)
-            evens = new_front
-        
-        timer += 1
-    return edges
-
-def time_to_reach_corners(grid, start_point):
-    # target is edges
-    timer = 0
-    evens = {start_point}
-    evens_total = 0
-    
-    odds = set()
-    odds_total = 0
-
-    corners = dict()
-    while len(corners) < 10:
-        #if timer % 100 == 0: print(timer, '...', f'{len(odds)=} {len(evens)=}')
-        if timer % 2 == 0:
-            start_points = evens
-            other = odds
-        else:
-            start_points = odds
-            other = evens
-        new_front = set()
-        for sp in start_points:
-            #print(sp)
-            r, c = sp
-            if ((r < 0 or r >= height) and (c < 0 or c >= width)) and (r, c) not in corners:
-                corners[(r, c)] = timer
-                continue
-            for dr, dc in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
-                if (
-                    (((r + dr), (c + dc)) not in other)
-                    and (grid[(((r + dr) % height), ((c + dc) % width))] == '.')
-                ):
-                    new_front.add((r + dr, c + dc))
-        if timer % 2 == 0:
-            odds_total += len(odds)
-            odds = new_front
-        else:
-            evens_total += len(evens)
-            evens = new_front
-        
-        timer += 1
-    return corners
-
-# Every move goes from a black square onto a white square or vice versa.
-# Hence, once a square is marked as "odd", it will show up in the final count.
-
-
-'''
-evens, odds = even_odd_floodfill(grid, start, 100) # 6536
-print(evens)
-evens, odds = even_odd_floodfill(grid, start, 500) # 167004
-print(evens)
-evens, odds = even_odd_floodfill(grid, start, 1000) # 668697
-print(evens)
-evens, odds = even_odd_floodfill(grid, start, 5000) # 16733044
-print(evens)
-'''
-
-evens, odds = even_odd_floodfill(grid, start, 1375)
-print(odds)
-
-PART_1 = 64
+PART_1 = 64 # soln 3853
 PART_2 = 26501365 # = 202300 * 131 + 65 (where 131 is height and 65 is dist to first edge)
 
-# 639 133 389 136 005
-# is too high
+res = floodfill(grid, start, PART_1)
+print(count_of(res, ('E' if PART_1 % 2 == 0 else 'O')))
 
-# wolframalpha's quadratic approximation is
-# 638 238 358 607 363
-# but this is too low
+#PART_2 = 458 # 131 * 3 + 65 => should return 191585
+#PART_2 = 1375 # 131 * 10 + 65 => should return 1722471
+#PART_2 = 65 # should return 3853
+
+start_r, start_c = start
+
+#PART_2 = height * 4 + start_r
+multiplier = (PART_2 - start_r) // height
+#res = floodfill(grid, start, PART_2)
+#print(count_of(res, ('E' if PART_2 % 2 == 0 else 'O')))
+
+
+'''
+ 1*1   * = corner piece
+17X71  1 = one-eighth piece
+*XXX*  7 = seven-eighths piece
+17X71  X = full piece
+ 1*1
+'''
+
+big_grid = floodfill(grid, start, start_r + (height * 2))
+# TODO make this not so lame
+full_tile = {
+    (r, c): v for (r, c), v in big_grid.items()
+    if (r >= 0 and r < height and c >= 0 and c < width)
+}
+upper_right_one_eighth = {
+    (r, c): v for (r, c), v in big_grid.items()
+    if (r >= (-2 * height) and r < (-1 * height) and c >= width and c < (2 * width))
+}
+upper_left_one_eighth = {
+    (r, c): v for (r, c), v in big_grid.items()
+    if (r >= (-2 * height) and r < (-1 * height) and c >= (-1 * width) and c < 0)
+}
+lower_left_one_eighth = {
+    (r, c): v for (r, c), v in big_grid.items()
+    if (r >= (2 * height) and r < (3 * height) and c >= (-1 * width) and c < 0)
+}
+lower_right_one_eighth = {
+    (r, c): v for (r, c), v in big_grid.items()
+    if (r >= (2 * height) and r < (3 * height) and c >= width and c < (2 * width))
+}
+upper_right_seven_eighths = {
+    (r, c): v for (r, c), v in big_grid.items()
+    if (r >= (-1 * height) and r < 0 and c >= width and c < (2 * width))
+}
+upper_left_seven_eighths = {
+    (r, c): v for (r, c), v in big_grid.items()
+    if (r >= (-1 * height) and r < 0 and c >= (-1 * width) and c < 0)
+}
+lower_left_seven_eighths = {
+    (r, c): v for (r, c), v in big_grid.items()
+    if (r >= height and r < (2 * height) and c >= (-1 * width) and c < 0)
+}
+lower_right_seven_eighths = {
+    (r, c): v for (r, c), v in big_grid.items()
+    if (r >= height and r < (2 * height) and c >= width and c < (2 * width))
+}
+top_corner = {
+    (r, c): v for (r, c), v in big_grid.items()
+    if (r >= (-2 * height) and r < (-1 * height) and c >= 0 and c < width)
+}
+right_corner = {
+    (r, c): v for (r, c), v in big_grid.items()
+    if (r >= 0 and r < height and c >= (2 * width) and c < (3 * width))
+}
+bottom_corner = {
+    (r, c): v for (r, c), v in big_grid.items()
+    if (r >= (2* height) and r < (3 * height) and c >= 0 and c < width)
+}
+left_corner = {
+    (r, c): v for (r, c), v in big_grid.items()
+    if (r >= 0 and r < height and c >= (-2 * width) and c < (-1 * width))
+}
+
+total = 0
+# odds
+for piece, count in [
+    # the seven-eighths might need to all be flipped
+    (lower_left_seven_eighths,  multiplier - 1),
+    (lower_right_seven_eighths, multiplier - 1),
+    (upper_left_seven_eighths,  multiplier - 1),
+    (upper_right_seven_eighths, multiplier - 1),
+    # the one-eighths might need to all be flipped
+    (lower_left_one_eighth,  multiplier),
+    (lower_right_one_eighth, multiplier),
+    (upper_left_one_eighth,  multiplier),
+    (upper_right_one_eighth, multiplier),
+    #
+    (top_corner,    1),
+    (bottom_corner, 1),
+    (left_corner,   1),
+    (right_corner,  1),
+    # full tiles
+    (full_tile, (multiplier - 1)**2)
+]:
+    total += (count * count_of(piece, 'O'))
+
+# evens
+total += ((multiplier ** 2) * count_of(full_tile, 'E'))
+
+print(total)
+
+# TODO offset everything so fewer unique pieces are required (8 instead of 12)
